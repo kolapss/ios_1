49592d25a2fc9e3a497afc3603a421f607b15e93
Alexey Laptev <125090010+kolapss@users.noreply.github.com>
2025-02-13 17:01:54 +0300

    Add files via upload

---
+++ main.swift
@@ -0,0 +1,14 @@
+let bigNumber = 1446647474
+
+let oct = 0o15
+let number = 30
+let hex = 0x554
+let binary = 0b11100
+
+
+let name = "Алексей"
+let age = 22
+let pstudy="ЛЭТИ"
+let course = 4
+let message = "Меня зовут \(name), мне \(age) лет. Я учусь в \(pstudy) на \(course) курсе "
+print(message) 
